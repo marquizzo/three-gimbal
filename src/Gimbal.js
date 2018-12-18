642009664da1dca51fbca6f81f8e14ec760b6ed2
Marquizzo <marco@dyadstudios.com>
2018-12-18 12:22:16 -0800

    Add example & transpile from .ts to .js.

--- src/Gimbal.js
+++ src/Gimbal.js
@@ -1,32 +1,19 @@
-
-class SensorData{
-    alpha: number = 0;
-    beta: number = 0;
-    gamma: number = 0;
-    orientation: number;
+class SensorData {
     constructor(){
-        this.orientation = window.orientation ? <number>window.orientation : 0;
+        this.alpha = 0;
+        this.beta = 0;
+        this.gamma = 0;
+        this.orientation = window.orientation ? window.orientation : 0;
     }
 }
 
 // Transforms accelerometer alpha, beta, gamma information,
 // then returns readable angles via yaw, pitch & roll
-export default class Gimbal{
-    readonly RAD: number = Math.PI / 180;
-    readonly DEG: number = 180 / Math.PI;
+// yaw: y-axis rotations
+// pitch: x-axis rotations
+// roll: z-axis rotations
 
-    eulerOrigin: THREE.Euler;
-    quaternion: THREE.Quaternion;
-    axisUp: THREE.Vector3;
-    axisFwd: THREE.Vector3;
-    sensorRotations: THREE.Object3D;
-    data: SensorData;
-
-    // Output
-    yaw: number = 0;        // y-axis rotations
-    pitch: number = 0;  // x-axis rotations
-    roll: number = 0;       // z-axis rotations
-
+class Gimbal {
     constructor(){
         this.quaternion = new THREE.Quaternion();
         this.axisUp = new THREE.Vector3(0, 1, 0);
@@ -34,35 +21,50 @@ export default class Gimbal{
         this.sensorRotations = new THREE.Object3D();
         this.data = new SensorData();
 
+        this.RAD = Math.PI / 180;
+        this.DEG = 180 / Math.PI;
+
         this.eulerOrigin = new THREE.Euler();
 
         if(typeof window.orientation !== "undefined"){
             this.eulerOrigin.set(
                 90 * this.RAD, 
                 180 * this.RAD, 
-                (180 + <number>window.orientation) * this.RAD
+                (180 + window.orientation) * this.RAD
             );
         }
     }
 
-    public enable():void{
+    enable() {
         this.onDeviceReorientation();
 
         window.addEventListener("deviceorientation", this.onSensorMove.bind(this), false);
         window.addEventListener("orientationchange", this.onDeviceReorientation.bind(this), false);
     }
 
-    public disable():void{
+    disable() {
         window.removeEventListener("deviceorientation", this.onSensorMove.bind(this), false);
         window.removeEventListener("orientationchange", this.onDeviceReorientation.bind(this), false);
     }
 
     //////////////////////////////////////////// EVENT LISTENERS  /////////////////////////////////////////////
-    public update():void{
+    // When switching from portrait <---> landscape
+    onDeviceReorientation() {
+        this.data.orientation = (window.orientation * this.RAD) || 0;
+    }
+
+    // Update data when device detects movement
+    onSensorMove(event) {
         // Alpha = z axis [0 ,360]
         // Beta = x axis [-180 , 180]
         // Gamma = y axis [-90 , 90]
+        this.data.alpha = event.alpha;
+        this.data.beta = event.beta;
+        this.data.gamma = event.gamma;
+    }
 
+    // Called once per frame
+    update() {
         // Reset rotation
         this.sensorRotations.setRotationFromEuler(this.eulerOrigin);
 
@@ -89,15 +91,5 @@ export default class Gimbal{
 
         // Roll is left (-) or right (+) rotation around local z-axis
         this.roll = Math.atan2(-this.axisUp.x, this.axisUp.y);
-    }
-
-    private onDeviceReorientation():void{
-        this.data.orientation = (<number>window.orientation * this.RAD) || 0;
-    }
-
-    private onSensorMove(event: DeviceOrientationEvent):void{
-        this.data.alpha = event.alpha;
-        this.data.beta = event.beta;
-        this.data.gamma = event.gamma;
     }
 }
